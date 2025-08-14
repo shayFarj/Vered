7ede5c556c32342b78f378872b5b31cade81d59a
shayFarj <shayfarjoon0@gmail.com>
2025-08-14 09:53:29 +0300

    Making sure cascade boards aren't using dangling pointers of cascades.

--- Vered/InstBoard.cpp
+++ Vered/InstBoard.cpp
@@ -230,6 +230,7 @@ void InstBoard::render()
 				this->inst.eraseCas(this->cRow1);
 			else
 				delete this->cells[this->cCol1][this->cRow1].cas;
+			this->cBoardR.setCas(nullptr);
 
 			this->cells[this->cCol1].erase(this->cells[this->cCol1].begin() + this->cRow1);
 			PaStreamer::unpauseStream();
@@ -254,6 +255,7 @@ void InstBoard::render()
 				this->inst.eraseCas(this->cRow2);
 			else
 				delete this->cells[this->cCol2][this->cRow2].cas;
+			this->cBoardB.setCas(nullptr);
 			this->cells[this->cCol2].erase(this->cells[this->cCol2].begin() + this->cRow2);
 			PaStreamer::unpauseStream();
 		}
