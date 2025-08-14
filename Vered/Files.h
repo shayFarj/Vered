17ebc637dc02f6b8da9f484cec6f5caf0e73144a
shayFarj <shayfarjoon0@gmail.com>
2025-08-14 16:56:49 +0300

    Creating static class for managing loading and saving instruments

---
+++ Vered/Files.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Instrument.h"
+static class Files
+{
+public:
+
+private:
+	
+	static void saveInst(Instrument inst);
+	static Instrument* loadInst(const char* filepath);
+
+};
+
