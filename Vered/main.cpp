48f92bc62b8e1ad097861fb2d23e7c94966c5e9b
shayFarj <shayfarjoon0@gmail.com>
2025-08-22 15:54:56 +0300

    Changed window title

--- Vered/main.cpp
+++ Vered/main.cpp
@@ -86,7 +86,7 @@ int main(int, char**)
 
     // Create window with graphics context
     float main_scale = ImGui_ImplGlfw_GetContentScaleForMonitor(glfwGetPrimaryMonitor()); // Valid on GLFW 3.3+ only
-    GLFWwindow* window = glfwCreateWindow((int)(1280 * main_scale), (int)(800 * main_scale), "bababooey", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow((int)(1280 * main_scale), (int)(800 * main_scale), "Vered", nullptr, nullptr);
 
     
     
