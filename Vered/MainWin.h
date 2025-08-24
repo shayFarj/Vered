50e4b2563641d8ddbe46149196ce2034d7e61b63
shayFarj <shayfarjoon0@gmail.com>
2025-08-24 20:23:33 +0300

    Removing unnecassary variables from mainWin

--- Vered/MainWin.h
+++ Vered/MainWin.h
@@ -21,33 +21,9 @@ private:
 	InstBoard iBoard;
 
 	LiveMixer mixer;
-
-	Cascade cas;
-
-	Cascade cas2;
-	Cascade cas3;
-
-	Operator op4;
-	Operator op5;
-	Operator op6;
-
-
-	Operator op3;
-	Operator op1;
-	Operator op2;
-
-	Instrument inst;
-
-	Instrument inst1;
-	Instrument inst2;
-
-	FourierCalc fc_1;
-	FourierCalc fc_2;
-	FourierCalc fc_3;
 	
 	FourierWin fWin;
 
-	bool showGraph = false;
 	bool havePlayed = false;
 	bool resetKeyTime = false;
 
